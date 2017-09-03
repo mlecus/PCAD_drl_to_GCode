b1f88d42b712eafa4e87f28aa8b0bcff05ca0e97
mlecus <mlecus@tut.by>
2017-09-03 12:21:21 +0300

    simplify parseExcellon

--- src/mlecus_tut/by/mlecus/ExcellonAnalyser/Excellon.java
+++ src/mlecus_tut/by/mlecus/ExcellonAnalyser/Excellon.java
@@ -18,14 +18,21 @@ public class Excellon {
     private int digBeforePoint=0;
     private int digAfterPoint=0;
     private FileOpration f;
+    private ILogWriter log;
+    private String s;
     //private DrillFrame drillFrame;
     //private DrillToolMap drillToolMap;
 
-    public  Excellon ()
-    {}
+    public  Excellon (String fileName)
+    {
+        log= new ConsoleLogWriter();
+        log.OpenLog("");
+        f=new FileOpration();
+        f.Open(fileName);
+    }
     private String SkipComments()
     {
-        String s;
+        //String s;
         do {
             s = f.Readln();
             if (s == null) return s;
@@ -33,15 +40,10 @@ public class Excellon {
         while (s.charAt(0)==';');
         return s;
     }
-    public boolean ParseExcellon(String fileName, DrillList drillList, DrillToolMap drillToolMap)
+    public boolean ParseHeader (DrillToolMap drillToolMap)
     {
-        //this.drillToolMap = drillToolMap;
-        //this.drillFrame = drillFrame;
-        ILogWriter log= new ConsoleLogWriter();
-        f=new FileOpration();
-        f.Open(fileName);
-        String s;
-         s=SkipComments();
+        //String s;
+        s=SkipComments();
         if (s.compareTo("M48")!=0)
         {
             log.Writeln("begin of header not found");
@@ -65,7 +67,7 @@ public class Excellon {
         catch (NumberFormatException | IndexOutOfBoundsException exc)
         {
             log.Writeln("invalid decimal point position declaration");
-            f.Close();
+            //f.Close();
             return false;
         }
         /*catch (IndexOutOfBoundsException exc)
@@ -105,52 +107,29 @@ public class Excellon {
             catch (NumberFormatException | IndexOutOfBoundsException exc)
             {
                 log.Writeln("invalid tool declaration at "+f.getIndex());
-                f.Close();
+                //f.Close();
                 return false;
             }
 
         } while (s.charAt(0)!='%');
         log.Writeln("end of header found at "+f.getIndex());
-        s = SkipComments();
-        do {
-            if (s==null)
-            {
-                log.Writeln("parsing completed without footer at "+f.getIndex());
-                f.Close();
-                return false;
-            }
-            if (s.compareTo("T00")==0)
-            {
-                log.Writeln("\tFooter found at "+f.getIndex());
-                s = SkipComments();
-                log.Writeln(s);
-                if (s.compareTo("M30")==0)
-                {
-                    log.Writeln("\tEnd of file found at "+f.getIndex());
-                    f.Close();
-                    log.Writeln("\tPARSING COMPLETED");
-                    return true;
-                }
-                log.Writeln("parsing completed without footer at "+f.getIndex());
-                f.Close();
-                return false;
-            }
+        return true;
+    }
+
+    public boolean ParseDrillFrame (DrillToolMap drillToolMap, DrillFrame df)
+    {
+
+        //String s="";
+        //DrillFrame df=new DrillFrame();
+        df.setDrillDiam(drillToolMap.GetToolDiam(s));
+        log.Writeln("\ttool "+s+" frame with tool diameter "+ df.getDrillDiam()+" found at "+f.getIndex());
+        double x=0,y=0;
+        do
+        {
+            s=SkipComments();
             log.Writeln(s);
-            if (drillToolMap.GetToolDiam(s) == -1) {
-                log.Writeln("invalid tool name found at " + f.getIndex());
-                f.Close();
-                return false;
-            }
-            DrillFrame df=new DrillFrame();
-            df.setDrillDiam(drillToolMap.GetToolDiam(s));
-            log.Writeln("\ttool "+s+" frame with tool diameter "+ df.getDrillDiam()+" found at "+f.getIndex());
-            double x=0,y=0;
-            do
+            try
             {
-                s=SkipComments();
-                log.Writeln(s);
-                try
-                {
                 String tempX="",tempY="";
                 if (s.indexOf('X')!=-1)
                 {
@@ -172,26 +151,85 @@ public class Excellon {
                     }
                     else
                     {
-                        drillList.Add(df);
-                        break;
+                        //drillList.Add(df);
+                        //break;
+                        return true;
                     }
 
                 }
                 if (tempX!="")
-                x=Double.parseDouble(tempX)/Math.pow(10,digAfterPoint);
+                    x=Double.parseDouble(tempX)/Math.pow(10,digAfterPoint);
                 if (tempY!="")
-                y=Double.parseDouble(tempY)/Math.pow(10,digAfterPoint);
+                    y=Double.parseDouble(tempY)/Math.pow(10,digAfterPoint);
                 df.addPoint(x,y);
                 log.Writeln("\tpoint ("+Double.toString(x)+","+Double.toString(y)+") found at "+f.getIndex());
-                }
-                catch (NumberFormatException | IndexOutOfBoundsException exc)
-                {
-                    log.Writeln("invalid coordinate at "+f.getIndex());
-                    f.Close();
-                    return false;
-                }
+            }
+            catch (NumberFormatException | IndexOutOfBoundsException exc)
+            {
+                log.Writeln("invalid coordinate at "+f.getIndex());
+                //f.Close();
+                return false;
+            }
 
-            }while(true);
+        }while(true);
+        //return true;
+    }
+    public boolean ParseFooter()
+    {
+        log.Writeln("\tFooter found at "+f.getIndex());
+        s = SkipComments();
+        log.Writeln(s);
+        if (s.compareTo("M30")==0)
+        {
+            log.Writeln("\tEnd of file found at "+f.getIndex());
+            //f.Close();
+            log.Writeln("\tPARSING COMPLETED");
+            return true;
+        }
+        log.Writeln("parsing completed without footer at "+f.getIndex());
+        //f.Close();
+        return false;
+        //return true;
+    }
+    public boolean ParseExcellon(String fileName, DrillList drillList, DrillToolMap drillToolMap)
+    {
+        if (!ParseHeader(drillToolMap))
+        {
+            f.Close();
+            return  false;
+        }
+
+        s = SkipComments();
+        do {
+            if (s==null)
+            {
+                log.Writeln("parsing completed without footer at "+f.getIndex());
+                f.Close();
+                log.CloseLog();
+                return false;
+            }
+            if (s.compareTo("T00")==0)
+            {
+                boolean exitCode=ParseFooter();
+                f.Close();
+                return exitCode;
+            }
+            log.Writeln(s);
+            if (drillToolMap.GetToolDiam(s) == -1) {
+                log.Writeln("invalid tool name found at " + f.getIndex());
+                f.Close();
+                log.CloseLog();
+                return false;
+            }
+
+            DrillFrame df=new DrillFrame();
+            if (!ParseDrillFrame(drillToolMap,df))
+            {
+                f.Close();
+                return false;
+            };
+            drillList.Add(df);
+
         }while (true);
 
 
