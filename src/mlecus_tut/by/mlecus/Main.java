525884b719dd993bd11b4dd3d067fbfceefce4d3
mlecus <mlecus@tut.by>
2017-09-04 20:59:52 +0300

    simplify parseExcellon

--- src/mlecus_tut/by/mlecus/Main.java
+++ src/mlecus_tut/by/mlecus/Main.java
@@ -6,10 +6,6 @@ import mlecus_tut.by.mlecus.ExcellonAnalyser.Excellon;
 
 public class Main {
 
-    static public void writeln(Object ob)
-    {
-        System.out.println(ob);
-    }
     public static void main(String[] args) {
          Excellon excellon=new Excellon("data/TEST.DRL");
         //Excellon excellon=new Excellon("data/NTC-15-01-13_MAINBOARD_V1.2.DRL");
